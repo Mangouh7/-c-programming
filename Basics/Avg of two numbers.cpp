d30d78da59698f24cb4d1906887e818d8d1c6639
Aascharya paudel <153407432+Mangouh7@users.noreply.github.com>
2025-11-08 11:29:07 +0545

    Add files via upload

---
+++ Basics/Avg of two numbers.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,avg;
+	printf("Enter two numbers\n");
+	scanf("%d%d",&a,&b);
+	avg = (a+b)/2;
+	printf("the average of two numbers is %d",avg);
+	return 0;
+}
