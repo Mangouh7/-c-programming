4519c92892840efb4a2960305126971ad7a478e9
Aascharya paudel <153407432+Mangouh7@users.noreply.github.com>
2025-11-08 09:05:08 +0545

    Add files via upload

---
+++ Basics/calculator.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main ()
+{
+	int a,b,A,m,d,s;
+	printf("enter two numbers  number\n");
+	scanf("%d%d", &a, &b);
+	A = a+b;
+	printf("the sum of two numbers is %d",A);
+	s = a-b;
+	printf("The subtraction of these two numbers is %d\n",s);
+	
+	m =  a * b;
+	printf("the multipication of two numbers is %d\n",m);
+	
+	d = a / b;
+	printf("the divison of two numbers is %d\n",d);
+	
+	
+	
+	
+	
+	return 0;
+	
+}
